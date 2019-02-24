f6664967259524349be6e4f0bd24a27eb54792c7
Gari Hari <xc2a@pop3.mail-box.ne.jp>
2019-02-24 16:02:10 +0900

    del .nav-tabs border-color

--- darkside/css/index.php
+++ darkside/css/index.php
@@ -114,7 +114,7 @@ else echo '.page-top{padding-top:2rem!important;margin-bottom:2rem!important}';
 if ($color) echo '
 .form-control,.page-link{background-color:'. hsla($color, 0, 0, .05). '}
 .page-item.active .page-link{border-color:white}
-.nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active{background-color:'. hsla($color, 0, 0, .05). ';border-color:#dee2e6 #dee2e6 #3a3a40!important}
+.nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active{background-color:'. hsla($color, 0, 0, .05). '}
 ::selection, .highlight, .comment:target{background-color:'.hsla($color, 0, 0, .2).';color:'.hsla($color).'}
 ::-moz-selection, .highlight, .comment:target{background-color:'.hsla($color, 0, 0, .2).';color:'.hsla($color).'}
 ';
