71ccc324b6e06e62f7a9447429d449138fb335b1
DOlDNa <xc2a@pop3.mail-box.ne.jp>
2019-03-14 11:13:54 +0900

    Update sideboxes.php

--- simpletron/sideboxes.php
+++ simpletron/sideboxes.php
@@ -3,14 +3,14 @@ if ($use_recents && $recent_files = glob($glob_dir. 'index.html', GLOB_NOSORT))
 {
 	usort($recent_files, 'sort_time');
 
-	$aside .= '<div id=recents class="mb-5"><h5>'. $recents. '</h5>'. $n;
+	$aside .= '<div id=recents class="mb-5"><h2 class=h5>'. $recents. '</h2>'. $n;
 	$j = 0;
 	foreach($recent_files as $recents_name)
 	{
 		if ($j === $number_of_recents) break;
 		$recent_categ = get_categ($recents_name);
 		$recent_title = get_title($recents_name);
-		$aside .= '<a class="'. ($categ_name. $title_name === $recent_categ. $recent_title ? 'current' : ''). '" href="'. $url. r($recent_categ. '/'. $recent_title). '">'. h($recent_title). '</a>'. $n;
+		$aside .= '<a'. ($categ_name. $title_name === $recent_categ. $recent_title ? ' class=current' : ''). ' href="'. $url. r($recent_categ. '/'. $recent_title). '">'. h($recent_title). '</a>'. $n;
 		++$j;
 	}
 	$aside .= '</div>';
@@ -21,27 +21,27 @@ if ($glob_info_files || $dl || $use_contact && $mail_address)
 {
 	usort($glob_info_files, 'sort_time');
 
-	$aside .= '<div id=informations class="mb-5"><h5>'. $informations. '</h5>'. $n;
+	$aside .= '<div id=informations class="mb-5"><h2 class=h5>'. $informations. '</h2>'. $n;
 
 	foreach($glob_info_files as $info_files)
 	{
 		if ($info_files === 'contents/index.html') continue;
 		$info_uri = basename($info_files, '.html');
-		$aside .= '<a class="'. ($page_name === $info_uri ? 'current' : ''). '" href="'. $url. r($info_uri). '">'. h($info_uri). '</a>'. $n;
+		$aside .= '<a'. ($page_name === $info_uri ? ' class=current' : ''). ' href="'. $url. r($info_uri). '">'. h($info_uri). '</a>'. $n;
 	}
 
 	if ($dl)
-		$aside .= '<a class="'. ($page_name === $download_contents ? 'current' : ''). '" href="'. $url. r($download_contents). '">'. $download_contents. '</a>'. $n;
+		$aside .= '<a'. ($page_name === $download_contents ? ' class=current' : ''). ' href="'. $url. r($download_contents). '">'. $download_contents. '</a>'. $n;
 
 	if ($use_contact && $mail_address)
-		$aside .= '<a class="'. ($page_name === $contact_us ? 'current' : ''). '" href="'. $url. r($contact_us). '">'. $contact_us. '</a>'. $n;
+		$aside .= '<a'. ($page_name === $contact_us ? ' class=current' : ''). ' href="'. $url. r($contact_us). '">'. $contact_us. '</a>'. $n;
 	$aside .= '</div>';
 }
 
 if ($address)
 	$aside .=
 	'<div id=address class="mb-5">'. $n.
-	'<h5>'. ($address_title ? $address_title : $site_name). '</h5>'. $n.
+	'<h2 class=h5>'. ($address_title ? $address_title : $site_name). '</h2>'. $n.
 	'<div class="wrap">'. $address. '</div>'. $n.
 	'</div>';
 
@@ -49,7 +49,7 @@ if ($use_popular_articles && $number_of_popular_articles > 0 &&$glob_all_counter
 {
 	$aside .=
 	'<div id=popular-articles class="mb-5">'. $n.
-	'<h5>'. $popular_articles. '</h5>'. $n;
+	'<h2 class=h5>'. $popular_articles. '</h2>'. $n;
 
 	foreach($glob_all_counter_files as $all_counter_files)
 		$counter_sort[] = (int)trim(file_get_contents($all_counter_files)). $all_counter_files;
@@ -60,7 +60,7 @@ if ($use_popular_articles && $number_of_popular_articles > 0 &&$glob_all_counter
 	{
 		$popular_titles = explode('/', $counter_sort[$i]);
 		$aside .=
-		'<a class="'. ($categ_name. $title_name === $popular_titles[1]. $popular_titles[2] ? 'current' : ''). '" href="'. $url. r($popular_titles[1]. '/'. $popular_titles[2]). '">'. h($popular_titles[2]). '</a>'. $n;
+		'<a'. ($categ_name. $title_name === $popular_titles[1]. $popular_titles[2] ? ' class=current' : ''). ' href="'. $url. r($popular_titles[1]. '/'. $popular_titles[2]). '">'. h($popular_titles[2]). '</a>'. $n;
 	}
 	$aside .= '</div>'. $n;
 }
@@ -71,7 +71,7 @@ if ($use_comment && $number_of_new_comments > 0 && $glob_all_comment_files = glo
 
 	$aside .=
 	'<div id=recent-comments class="mb-5">'. $n.
-	'<h5>'. $recent_comments. '</h5>';
+	'<h2 class=h5>'. $recent_comments. '</h2>';
 
 	$j = 0;
 	foreach($glob_all_comment_files as $all_comments)
