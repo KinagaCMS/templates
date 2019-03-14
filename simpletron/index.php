138fe08f0c30ff15391ecc07b39cdcb762764ee7
DOlDNa <xc2a@pop3.mail-box.ne.jp>
2019-03-14 11:11:04 +0900

    Update index.php

--- simpletron/index.php
+++ simpletron/index.php
@@ -8,7 +8,7 @@
 </head>
 <body id=TOP>
 <nav class="justify-content-center navbar navbar-expand-lg navbar-dark bg-primary rounded-0">
-<button class=navbar-toggler data-toggle=collapse data-target="#nav" aria-controls=nav aria-expanded=false><span class=navbar-toggler-icon></span></button>
+<button class=navbar-toggler data-toggle=collapse data-target="#nav" aria-controls=nav aria-expanded=false accesskey=n tabindex=100><span class=navbar-toggler-icon></span></button>
 <ul class="justify-content-center navbar-nav collapse navbar-collapse" id=nav><?=$nav?></ul>
 </nav>
 <header class="jumbotron mh-100 text-center rounded-0">
@@ -21,7 +21,7 @@
 <div class=container><?=$article?></div>
 <aside class="container-fluid d-flex flex-wrap flex-column flex-md-row justify-content-around bg-primary text-white pt-5"><?=$aside?></aside>
 <footer class="text-center bg-primary text-white pb-3">
-<a href="#TOP" id=page-top class="btn btn-outline-primary bg-primary"><svg width="2em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="m8 3-7 9h5l2-3 2 3h5l-7-9z" fill="currentColor"/></svg></a>
+<a href="#TOP" id=page-top class="btn btn-outline-primary bg-primary"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="m8 3-7 9h5l2-3 2 3h5l-7-9z" fill="currentColor"/></svg></a>
 <script src="<?=$js?>"></script>
 <?=$footer?>
 </footer>
