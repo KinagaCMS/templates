2b11494ad2a1864d8fbe64c82a3d8062e3d078ac
DOlDNa <info@garihari.sakura.ne.jp>
2020-10-17 15:26:53 +0900

    Update index.php

--- classic/index.php
+++ classic/index.php
@@ -24,7 +24,7 @@
 <div id=side class="col-lg-3 d-flex flex-column"><?=$aside?></div>
 </div>
 </div>
-<footer class="text-center pb-3">
+<footer class="pb-3">
 <a href="#TOP" id=page-top class="btn btn-outline-primary">
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="currentColor" d="m2.5872 45.447-2.5872-2.043 24.034-40.851 23.966 40.987-2.996 1.362-21.038-35.745z"/></svg>
 </a>
