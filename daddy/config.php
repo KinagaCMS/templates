809fc4f82d02a9b0a6416c7e0f327e0e2ec68f5a
DOlDNa <info@garihari.sakura.ne.jp>
2020-10-17 15:33:34 +0900

    Update config.php

--- daddy/config.php
+++ daddy/config.php
@@ -1,3 +1,4 @@
 <?php
+$copyright = '<a href="http://shimizumari.com/fuwa2li/" target="_blank" rel="noopener noreferrer"><img src="'. $css. 'fuwa_b20.gif" alt="fuwa_b20" height=31 width=31></a>';
 $sidebox_order[0] = 0;
 $sidebox_wrapper_class[0] = 'list-group mb-3';
