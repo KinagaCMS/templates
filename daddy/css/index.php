fbdf34e113136414b7669f5a77d6fcfd1fd51a75
DOlDNa <info@garihari.sakura.ne.jp>
2022-02-20 16:12:53 +0900

    Add files via upload

--- daddy/css/index.php
+++ daddy/css/index.php
@@ -1,44 +1,48 @@
 <?php
 include '../../../includes/functions.php';
 include '../../../includes/config.php';
-
+header('Last-Modified: '. $last_modified = timestamp(__FILE__));
+if ($last_modified === filter_input(INPUT_SERVER, 'HTTP_IF_MODIFIED_SINCE')) header(filter_input(INPUT_SERVER, 'SERVER_PROTOCOL'). ' 304 Not Modified');
+header('Content-Type: text/css; charset='. $encoding);
 $bootstrap = file_get_contents('bootstrap.min.css');
-
 if ($color)
 {
 	#primary
-	$bootstrap = str_replace('#007bff', hsla($color, 5.5, -7, .9), $bootstrap);
-	#secondary
-	#$bootstrap = str_replace('#6c757d', hsla($color, -30, -10), $bootstrap);
-	#light
-	$bootstrap = str_replace('#f8f9fa', hsla($color, 5, 5, .1), $bootstrap);
+	$bootstrap = str_replace('#0d6efd', hsla($color, 0, -5), $bootstrap);
+	$bootstrap = str_replace('13,110,253,', 'var(--bs-primary-rgb),', $bootstrap);
+	#border, a:hover, btn-primary:hover, .btn-primary:focus .btn-primary:active, .btn-link:hover, .nav-link:hover, .nav-link:focus, .page-link:hover|focus, .link-primary:hover, .link-primary:focus
+	$bootstrap = str_replace(['#0b5ed7', '#0a58ca', '#0a53be'], hsla($color, 0, 0, 1), $bootstrap);
+	#.btn-primary:hover, .btn-primary:focus
+	$bootstrap = str_replace(['rgba(49,132,253,.5)', 'rgba(13,110,253,.5)', 'rgba(13,110,253,.25)'], hsla($color, 0, 5, .2), $bootstrap);
+	#alert-bg, .list-group-item-primary-bg .table-primary-bg
+	$bootstrap = str_replace(['#cfe2ff', '#b6d4fe'], hsla($color, 0, 15), $bootstrap);
+	#.alert-primary-color, .list-group-item-primary.list-group-item-action.active, input:focus
+	$bootstrap = str_replace(['#084298', '#86b7fe'], hsla($color, 0, -15), $bootstrap);
+	#.accordion-button:not(.collapsed)-bg
+	$bootstrap = str_replace(['#e7f1ff', '#bacbe6'], hsla($color, 0, 25), $bootstrap);
+	#.accordion-button:not(.collapsed)-color, .alert-primary .alert-link-color
+	$bootstrap = str_replace(['#0c63e4', '%230c63e4', '#06357a'], hsla($color, -25, -20), $bootstrap);
 	#dark
-	$bootstrap = str_replace('#343a40', hsla($color, -20, -10), $bootstrap);
-	#warning
-	$bootstrap = str_replace('#ffc107', 'hsla(50, 100%, 47%)', $bootstrap);
-	#*-primary
-	$bootstrap = str_replace('#b8daff', hsla($color, -5, -5), $bootstrap);
-	#breadcrumb, focus
-	$bootstrap = str_replace(['#dae0e5', '#e9ecef', 'rgba(0,123,255,.5)', 'rgba(0,123,255,.25)', 'rgba(128,189,255,.5)', 'rgba(38,143,255,.5)'], hsla($color, 0, 0, .05), $bootstrap);
-	#hover
-	$bootstrap = str_replace('#0056b3', hsla($color, 0, 0, .9), $bootstrap);
-	#btnhover
-	$bootstrap = str_replace(['#0069d9', '#0062cc'], hsla($color, -15, -15), $bootstrap);
-	#list link
-	$bootstrap = str_replace(['#495057', '#004085', '#002752'], hsla($color, -15, -15), $bootstrap);
-	#alert
-	$bootstrap = str_replace('#cce5ff', hsla($color, 0, 10), $bootstrap);
-	#border
-	$bootstrap = str_replace(['#7abaff', '#80bdff', '#005cbf'], hsla($color, 0, 20), $bootstrap);
+	$bootstrap = str_replace('#212529', '#3c3c3c', $bootstrap);
+
+	$c = hsla($color);
+	$c2 = hsla($color, 0, 0, .2);
+	$c05 = hsla($color, 0, -50, .3);
+	[$h, $s, $l] = explode(',', str_replace(['hsla', '(', ')', '%'], '', hsla($color, 0, -5)));
 }
-
-header('Content-Type: text/css; charset='. $encoding);
-header('Last-Modified: '. $last_modified = timestamp(__FILE__));
-
-if (filter_input(INPUT_SERVER, 'HTTP_IF_MODIFIED_SINCE') === $last_modified) header('HTTP/1.1 304 Not Modified');
-
-echo $bootstrap,
-file_get_contents('jquery.fancybox.min.css'), '
+else
+{
+	$bootstrap = str_replace('#e9ecef', 'rgba(0, 0, 0, .5)', $bootstrap);
+}
+$serif_fonts = '';
+if ('ja' === $lang)
+{
+	$bootstrap = str_replace('system-ui,', 'TakaoPGothic,"Noto Sans CJK JP",system-ui,', $bootstrap);
+	$bootstrap = str_replace('SFMono-Regular,', 'TakaoGothic,"Noto Sans Mono CJK JP",SFMono-Regular,', $bootstrap);
+	$serif_fonts = 'TakaoPMincho,"Noto Serif CJK JP",';
+}
+$get_categ = !filter_has_var(INPUT_GET, 'categ') ? '' : filter_input(INPUT_GET, 'categ', FILTER_CALLBACK, ['options' => 'strip_tags_basename']);
+echo $bootstrap, file_get_contents('fancybox.css'), (!$color ? '' : ':root{--bs-primary-rgb:'. implode(',', hsl2rgb($h, $s, $l)). '}'), 'html{scroll-behavior:smooth}a{text-decoration:none}#page-top svg{bottom:1em;position:fixed;right:1em;transition:all .3s linear .3s}
 .not-found{background-image:url(daddy13.gif);background-repeat:no-repeat;background-position: 50% 100%;min-height:350px}
 #page-top{bottom:2em;position:fixed;right:2em;display:none;z-index:10}
 #page-top svg{width:1em}
@@ -50,7 +54,7 @@ file_get_contents('jquery.fancybox.min.css'), '
 .bg-info.text-info{background-color:#d9edf7!important;color:#31708f!important}
 .bg-success.text-success{background-color:#dff0d8!important;color:#3c763d!important}
 .bg-warning.text-warning{background-color:#fcf8e3!important;color:#8a6d3b!important}
-.card-columns{column-count:1}
+
 .col-lg-4{flex:0 0 100%!important;max-width:100%!important}
 .index{flex-direction:column}.index div{width:100%!important}
 .comment:target{box-shadow:5px 5px 5px 3px rgba(0,0,0,.2)}
@@ -94,7 +98,7 @@ url{margin:1em auto;width:95%;display:block;font-size:small;padding:1em;overflow
 lastmod{display:block;text-align:right;margin-top:1em;white-space:nowrap}
 @media(max-width:991px){.card-columns{column-count:1}#form .input-group-text{display:block}}';
 
-if ($use_auto_wrap === true) echo '
+if ($use_auto_wrap) echo '
 .article,.card-text{word-wrap:break-word;white-space:pre-wrap}
 .article h1,.article h2,.article h3,.article h4,.article h5,.article h6{margin-bottom:0}
 .article #accordion,.article .accordion,.article .btn-group,.article .card,.article .carousel,.article .custom-control,.article .custom-file,.article .form-group,.article .form-horizontal,.article .input-group,.article .list-group,.article .media,.article .modal,.article .navbar,.article .row,.article .tab-content,.article dl,.article fieldset,.article li,.article ol,.article table,.article ul{white-space:normal!important}';
@@ -104,3 +108,7 @@ if ($color) echo '
 .nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active{background-color:'. hsla($color, 0, 0, .05). '}
 ::selection, .highlight{background-color:'.hsla($color, 0, 0, .2).';color:'.hsla($color).'}
 ::-moz-selection, .highlight{background-color:'.hsla($color, 0, 0, .2).';color:'.hsla($color).'}';
+
+if ($bootstrap4) echo '
+.badge{display:inline-block;padding:.25em .4em;font-size:75%;font-weight:700;line-height:1;text-align:center;white-space:nowrap;vertical-align:baseline;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out}@media (prefers-reduced-motion:reduce){.badge{transition:none}}a.badge:focus,a.badge:hover{text-decoration:none}.badge:empty{display:none}.btn .badge{position:relative;top:-1px}.badge-pill{padding-right:.6em;padding-left:.6em;border-radius:10rem}.badge-primary{color:#fff;background-color:#007bff}a.badge-primary:focus,a.badge-primary:hover{color:#fff;background-color:#0062cc}a.badge-primary.focus,a.badge-primary:focus{outline:0;box-shadow:0 0 0 .2rem rgba(0,123,255,.5)}.badge-secondary{color:#fff;background-color:#6c757d}a.badge-secondary:focus,a.badge-secondary:hover{color:#fff;background-color:#545b62}a.badge-secondary.focus,a.badge-secondary:focus{outline:0;box-shadow:0 0 0 .2rem rgba(108,117,125,.5)}.badge-success{color:#fff;background-color:#28a745}a.badge-success:focus,a.badge-success:hover{color:#fff;background-color:#1e7e34}a.badge-success.focus,a.badge-success:focus{outline:0;box-shadow:0 0 0 .2rem rgba(40,167,69,.5)}.badge-info{color:#fff;background-color:#17a2b8}a.badge-info:focus,a.badge-info:hover{color:#fff;background-color:#117a8b}a.badge-info.focus,a.badge-info:focus{outline:0;box-shadow:0 0 0 .2rem rgba(23,162,184,.5)}.badge-warning{color:#212529;background-color:#ffc107}a.badge-warning:focus,a.badge-warning:hover{color:#212529;background-color:#d39e00}a.badge-warning.focus,a.badge-warning:focus{outline:0;box-shadow:0 0 0 .2rem rgba(255,193,7,.5)}.badge-danger{color:#fff;background-color:#dc3545}a.badge-danger:focus,a.badge-danger:hover{color:#fff;background-color:#bd2130}a.badge-danger.focus,a.badge-danger:focus{outline:0;box-shadow:0 0 0 .2rem rgba(220,53,69,.5)}.badge-light{color:#212529;background-color:#f8f9fa}a.badge-light:focus,a.badge-light:hover{color:#212529;background-color:#dae0e5}a.badge-light.focus,a.badge-light:focus{outline:0;box-shadow:0 0 0 .2rem rgba(248,249,250,.5)}.badge-dark{color:#fff;background-color:#343a40}a.badge-dark:focus,a.badge-dark:hover{color:#fff;background-color:#1d2124}a.badge-dark.focus,a.badge-dark:focus{outline:0;box-shadow:0 0 0 .2rem rgba(52,58,64,.5)}.float-left{float:left!important}.float-right{float:right!important}@media (min-width:576px){.float-sm-left{float:left!important}.float-sm-right{float:right!important}.float-sm-none{float:none!important}}@media (min-width:768px){.float-md-left{float:left!important}.float-md-right{float:right!important}.float-md-none{float:none!important}}@media (min-width:992px){.float-lg-left{float:left!important}.float-lg-right{float:right!important}.float-lg-none{float:none!important}}@media (min-width:1200px){.float-xl-left{float:left!important}.float-xl-right{float:right!important}.float-xl-none{float:none!important}}.border-left{border-left:1px solid #dee2e6!important}.border-right-0{border-right:0!important}.border-left-0{border-left:0!important}.rounded-right{border-top-right-radius:.25rem!important;border-bottom-right-radius:.25rem!important}.rounded-left{border-top-left-radius:.25rem!important;border-bottom-left-radius:.25rem!important}.ml-0{margin-left:0!important}.ml-1{margin-left:.25rem!important}.ml-2{margin-left:.5rem!important}.ml-3{margin-left:1rem!important}.ml-4{margin-left:1.5rem!important}.ml-5{margin-left:3rem!important}.ml-auto{margin-left:auto!important}.mr-0{margin-right:0!important}.mr-1{margin-right:.25rem!important}.mr-2{margin-right:.5rem!important}.mr-3{margin-right:1rem!important}.mr-4{margin-right:1.5rem!important}.mr-5{margin-right:3rem!important}.mr-auto{margin-right:auto!important}.pl-0{padding-left:0!important}.pl-1{padding-left:.25rem!important}.pl-2{padding-left:.5rem!important}.pl-3{padding-left:1rem!important}.pl-4{padding-left:1.5rem!important}.pl-5{padding-left:3rem!important}.pl-auto{padding-left:auto!important}.pr-0{padding-right:0!important}.pr-1{padding-right:.25rem!important}.pr-2{padding-right:.5rem!important}.pr-3{padding-right:1rem!important}.pr-4{padding-right:1.5rem!important}.pr-5{padding-right:3rem!important}.pr-auto{padding-right:auto!important}.text-left{text-align:left!important}.text-right{text-align:right!important}
+';
