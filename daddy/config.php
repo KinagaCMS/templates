1e1622963d0f0faeac9d116d80d7691ad35976a4
DOlDNa <info@garihari.sakura.ne.jp>
2022-02-20 16:10:07 +0900

    Add files via upload

--- daddy/config.php
+++ daddy/config.php
@@ -1,4 +1,5 @@
 <?php
-$copyright = 'Illustrations by <a href="https://ringotomato.com/fuwa2li/" target="_blank" rel="noopener noreferrer">ふわふわ。り</a>';
-$sidebox_order[0] = 0;
-$sidebox_wrapper_class[0] = 'list-group mb-3';
+$copyright = '<a href="https://ringotomato.com/fuwa2li/" target="_blank" rel="noopener noreferrer">Illustrations by <img src="'. $url. 'templates/daddy/css/fuwa_b20.gif" width=31 height=31 alt=ふわふわ。り></a>';
+$sidebox_order[0] = 1;
+$sidebox_wrapper_class[0] = 'card shadow-sm mb-3';
+$index_column_class = $categ_column_class = $index_class = $categ_class = '';
--- daddy/index.php
+++ daddy/index.php
@@ -6,28 +6,26 @@
 <link href="<?=$css, ($get_categ ? '?categ='.$get_categ : '')?>" rel=stylesheet>
 <?=$header?>
 </head>
-<body id=TOP>
+<body id=TOP class=text-break>
 <div class=container>
-<header class="d-flex flex-column flex-md-row my-3 text-center">
+<header class="d-flex flex-column flex-md-row my-3 text-center" id=header>
 <div class="mx-auto w-50 order-md-2">
 <a href="<?=$url?>"><img class=img-fluid src="<?=$css?>daddy-<?=color2class($color)?>.gif" alt="daddy" height=315 width=123></a>
 </div>
 <div class="mx-auto text-md-left w-100 order-md-1">
+<ol class="breadcrumb justify-content-center mb-3"><?=$breadcrumb?></ol>
 <h1><a href="<?=$url?>"><?=get_logo()?></a></h1>
-<ul class="nav nav-pills justify-content-center justify-content-md-start mb-3"><?=$nav?></ul>
-<?php if ($meta_description):?>
-<p class="border border-primary rounded p-3 mb-3"><?=$meta_description?></p>
-<?php endif?>
-<?=$search?>
+<?php if ($meta_description):?><p class=my-4><?=$meta_description?></p><?php endif?>
+<div class=my-4><?=$search?></div>
+<div class="card shadow-sm p-3 my-3"><ul class="nav nav-pills"><?=$nav?></ul></div>
 </div>
 </header>
-<ol class="breadcrumb mb-3"><?=$breadcrumb?></ol>
-<div class="d-flex flex-column"><?=$aside?><div class=my-5><?=$article?></div></div>
-</div>
-<footer class="my-5">
-<a href="#TOP" id=page-top class="btn btn-outline-primary"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="currentColor" d="m2.5872 45.447-2.5872-2.043 24.034-40.851 23.966 40.987-2.996 1.362-21.038-35.745z"/></svg></a>
+
+<div class="d-flex flex-column" id=side><?=$aside?><?=$article?></div>
+<footer class="card shadow-sm p-3 my-3">
+<a href="#TOP" id=page-top><svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16"><path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/></svg></a>
 <script src="<?=$js?>"></script>
 <?=$footer?>
-</footer>
+</footer></div>
 </body>
 </html>
