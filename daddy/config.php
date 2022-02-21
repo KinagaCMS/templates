3e8b6f4217d79bf4172b2f2e7d4cebd6bb522e91
DOlDNa <info@garihari.sakura.ne.jp>
2022-02-21 14:34:51 +0900

    Add files via upload

--- daddy/config.php
+++ daddy/config.php
@@ -1,5 +1,5 @@
 <?php
-$copyright = '<a href="https://ringotomato.com/fuwa2li/" target="_blank" rel="noopener noreferrer">Illustrations by <img src="'. $url. 'templates/daddy/css/fuwa_b20.gif" width=31 height=31 alt=ふわふわ。り></a>';
+$copyright = '<a href="https://ringotomato.com/fuwa2li/" target="_blank" rel="noopener noreferrer">Illustrations by <img src="'. $css. 'fuwa_b20.gif" width=31 height=31 alt=ふわふわ。り></a>';
 $sidebox_order[0] = 1;
 $sidebox_wrapper_class[0] = 'card shadow-sm mb-3';
 $index_column_class = $categ_column_class = $index_class = $categ_class = '';
--- daddy/index.php
+++ daddy/index.php
@@ -20,7 +20,6 @@
 <div class="card shadow-sm p-3 my-3"><ul class="nav nav-pills"><?=$nav?></ul></div>
 </div>
 </header>
-
 <div class="d-flex flex-column" id=side><?=$aside?><?=$article?></div>
 <footer class="card shadow-sm p-3 my-3">
 <a href="#TOP" id=page-top><svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16"><path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/></svg></a>
