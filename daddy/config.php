e4729b5467dafcdbe05c8d6696832ce7907fb2c4
DOlDNa <info@garihari.sakura.ne.jp>
2022-02-25 13:39:24 +0900

    Update config.php

--- daddy/config.php
+++ daddy/config.php
@@ -1,5 +1,6 @@
 <?php
 $copyright = '<a href="https://ringotomato.com/fuwa2li/" target="_blank" rel="noopener noreferrer">Illustrations by <img src="'. $css. 'fuwa_b20.gif" width=31 height=31 alt=ふわふわ。り></a>';
+$use_info = true;
 $sidebox_order[0] = 1;
 $sidebox_wrapper_class[0] = 'card shadow-sm mb-3';
 $index_column_class = $categ_column_class = $index_class = $categ_class = '';
